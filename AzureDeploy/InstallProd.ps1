279181950593210b03bbee39167cb6c99a28d1ba
smacrico <smacricostas@gmail.com>
2019-11-28 15:35:56 +0200

    Update InstallProd.ps1

--- AzureDeploy/InstallProd.ps1
+++ AzureDeploy/InstallProd.ps1
@@ -1,16 +1,16 @@
 # Provision storage and function app
 .\Deploy-AzureResourceGroup.ps1 `
     -ResourceGroupLocation EastUS `
-    -ResourceGroupName OrchestratedProvisioningProd `
+    -ResourceGroupName pvresgroup `
     -TemplateParametersFile prod.parameters.json
 
 # Ensure storage queues are in place
-$storageAccount = Get-AzureRmStorageAccount | Where-Object ResourceGroupName -eq "OrchestratedProvisioningProd"
+$storageAccount = Get-AzureRmStorageAccount | Where-Object ResourceGroupName -eq "pvresgroup"
 $storageContext = $storageAccount.Context
-New-AzureStorageQueue -Name "create-team-request-queue" -Context $storageContext -ErrorAction Ignore
-New-AzureStorageQueue -Name "create-team-completion-queue" -Context $storageContext -ErrorAction Ignore
-New-AzureStorageQueue -Name "clone-team-request-queue" -Context $storageContext -ErrorAction Ignore
-New-AzureStorageQueue -Name "clone-team-completion-queue" -Context $storageContext -ErrorAction Ignore
+New-AzureStorageQueue -Name "create-team-request-devqueue" -Context $storageContext -ErrorAction Ignore
+New-AzureStorageQueue -Name "create-team-completion-devqueue" -Context $storageContext -ErrorAction Ignore
+New-AzureStorageQueue -Name "clone-team-request-devqueue" -Context $storageContext -ErrorAction Ignore
+New-AzureStorageQueue -Name "clone-team-completion-devqueue" -Context $storageContext -ErrorAction Ignore
 
 
 
