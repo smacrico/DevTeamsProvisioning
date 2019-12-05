35189177c9b3a6650646520d6077ef397f7f554e
smacrico <smacricostas@gmail.com>
2019-12-05 13:34:17 +0200

    LMH

--- AzureDeploy/Deploy-AzureResourceGroup.ps1
+++ AzureDeploy/Deploy-AzureResourceGroup.ps1
@@ -2,7 +2,7 @@
 
 Param(
     [string] [Parameter(Mandatory=$true)] $ResourceGroupLocation,
-    [string] $ResourceGroupName = 'devteamstorage',
+    [string] $ResourceGroupName = 'lmhteamstorage',
     [switch] $UploadArtifacts,
     [string] $StorageAccountName,
     [string] $StorageContainerName = $ResourceGroupName.ToLowerInvariant() + '-stageartifacts',
--- AzureDeploy/InstallDev.ps1
+++ AzureDeploy/InstallDev.ps1
@@ -1,11 +1,11 @@
 # Provision storage and function app
 .\Deploy-AzureResourceGroup.ps1 `
     -ResourceGroupLocation westeurope `
-    -ResourceGroupName devteamstorage `
+    -ResourceGroupName lmhteamstorage `
     -TemplateParametersFile prod.parameters.json
 
 # Ensure storage queues are in place
-$storageAccount = Get-AzureRmStorageAccount | Where-Object ResourceGroupName -eq "devteamstorage"
+$storageAccount = Get-AzureRmStorageAccount | Where-Object ResourceGroupName -eq "lmhteamstorage"
 $storageContext = $storageAccount.Context
 New-AzureStorageQueue -Name "create-team-request-stmaqueue" -Context $storageContext -ErrorAction Ignore
 New-AzureStorageQueue -Name "create-team-completion-stmaqueue" -Context $storageContext -ErrorAction Ignore
